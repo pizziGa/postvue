02acc9e85fd7b2bff9c47c23e0df75147036c887
pizziGa <gapizzi3@gmail.com>
2025-05-21 18:27:10 +0200

    possible fix

--- vuejs/vite.config.js
+++ vuejs/vite.config.js
@@ -5,6 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: {
+      origin: "https://instavue.onrender.com",
+      allowedHeaders: ["Content-Type", "Authorization"],
+      preflightContinue: true
+    }
+  },
   plugins: [
     vue(),
   ],
